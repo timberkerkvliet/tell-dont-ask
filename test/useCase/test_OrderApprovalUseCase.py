96d0caed756e15e478a8bcb5624c57addd8eb2a8
Timber Kerkvliet <timber@Timbers-MacBook-Air.local>
2022-11-21 20:21:51 +0100

    start

---
+++ test/useCase/test_OrderApprovalUseCase.py
@@ -0,0 +1,112 @@
+import unittest
+
+from src.domain.Order import Order
+from src.domain.OrderStatus import OrderStatus
+from src.useCase.ApprovedOrderCannotBeRejectedError import ApprovedOrderCannotBeRejectedError
+from src.useCase.OrderApprovalRequest import OrderApprovalRequest
+from src.useCase.OrderApprovalUseCase import OrderApprovalUseCase
+from src.useCase.RejectedOrderCannotBeApprovedError import RejectedOrderCannotBeApprovedError
+from src.useCase.ShippedOrdersCannotBeChangedError import ShippedOrdersCannotBeChangedError
+from test.doubles.TestOrderRepository import TestOrderRepository
+
+
+class TestOrderApprovalUseCase(unittest.TestCase):
+    def setUp(self):
+        self.order_repository = TestOrderRepository()
+        self.use_case = OrderApprovalUseCase(self.order_repository)
+
+    def test_approved_existing_order(self):
+        initial_order = Order()
+        initial_order.set_status(OrderStatus.CREATED)
+        initial_order.set_id(1)
+        self.order_repository.add_order(initial_order)
+
+        request = OrderApprovalRequest()
+        request.set_order_id(1)
+        request.set_approved(True)
+
+        self.use_case.run(request)
+
+        saved_order = self.order_repository.get_saved_order()
+
+        self.assertEqual(
+            OrderStatus.APPROVED,
+            saved_order.get_status()
+        )
+
+    def test_rejected_existing_order(self):
+        initial_order = Order()
+        initial_order.set_status(OrderStatus.CREATED)
+        initial_order.set_id(1)
+        self.order_repository.add_order(initial_order)
+
+        request = OrderApprovalRequest()
+        request.set_order_id(1)
+        request.set_approved(False)
+
+        self.use_case.run(request)
+
+        saved_order = self.order_repository.get_saved_order()
+
+        self.assertEqual(
+            OrderStatus.REJECTED,
+            saved_order.get_status()
+        )
+
+    def test_cannot_approve_rejected_order(self):
+        initial_order = Order()
+        initial_order.set_status(OrderStatus.REJECTED)
+        initial_order.set_id(1)
+        self.order_repository.add_order(initial_order)
+
+        request = OrderApprovalRequest()
+        request.set_order_id(1)
+        request.set_approved(True)
+
+        with self.assertRaises(RejectedOrderCannotBeApprovedError):
+            self.use_case.run(request)
+
+        self.assertIsNone(self.order_repository.get_saved_order())
+
+    def test_cannot_reject_approved_order(self):
+        initial_order = Order()
+        initial_order.set_status(OrderStatus.APPROVED)
+        initial_order.set_id(1)
+        self.order_repository.add_order(initial_order)
+
+        request = OrderApprovalRequest()
+        request.set_order_id(1)
+        request.set_approved(False)
+
+        with self.assertRaises(ApprovedOrderCannotBeRejectedError):
+            self.use_case.run(request)
+
+    def test_shipped_orders_cannot_be_approved(self):
+        initial_order = Order()
+        initial_order.set_status(OrderStatus.SHIPPED)
+        initial_order.set_id(1)
+        self.order_repository.add_order(initial_order)
+
+        request = OrderApprovalRequest()
+        request.set_order_id(1)
+        request.set_approved(True)
+
+        with self.assertRaises(ShippedOrdersCannotBeChangedError):
+            self.use_case.run(request)
+
+        self.assertIsNone(self.order_repository.get_saved_order())
+
+    def test_shipped_orders_cannot_be_rejected(self):
+        initial_order = Order()
+        initial_order.set_status(OrderStatus.SHIPPED)
+        initial_order.set_id(1)
+        self.order_repository.add_order(initial_order)
+
+        request = OrderApprovalRequest()
+        request.set_order_id(1)
+        request.set_approved(False)
+
+        with self.assertRaises(ShippedOrdersCannotBeChangedError):
+            self.use_case.run(request)
+
+        self.assertIsNone(self.order_repository.get_saved_order())
---
+++ test/useCase/test_OrderCreationUseCase.py
@@ -0,0 +1,81 @@
+import decimal
+import unittest
+
+from src.domain.Category import Category
+from src.domain.OrderStatus import OrderStatus
+from src.domain.Product import Product
+from src.useCase.OrderCreationUseCase import OrderCreationUseCase
+from src.useCase.SellItemRequest import SellItemRequest
+from src.useCase.SellItemsRequest import SellItemsRequest
+from src.useCase.UnknownProductError import UnknownProductError
+from test.doubles.InMemoryProductCatalog import InMemoryProductCatalog
+from test.doubles.TestOrderRepository import TestOrderRepository
+
+
+class TestOrderCreationUseCase(unittest.TestCase):
+    def setUp(self):
+        self.order_repository = TestOrderRepository()
+
+        food = Category()
+        food.set_name("food")
+        food.set_tax_percentage(decimal.Decimal("10"))
+
+        product1 = Product()
+        product1.set_name("salad")
+        product1.set_price(decimal.Decimal("3.56"))
+        product1.set_category(food)
+
+        product2 = Product()
+        product2.set_name("tomato")
+        product2.set_price(decimal.Decimal("4.65"))
+        product2.set_category(food)
+
+        productCatalog = InMemoryProductCatalog([product1, product2])
+        self.use_case = OrderCreationUseCase(self.order_repository, productCatalog)
+
+    def test_sell_multiple_items(self):
+        salad_request = SellItemRequest()
+        salad_request.set_product_name("salad")
+        salad_request.set_quantity(2)
+
+        tomato_request = SellItemRequest()
+        tomato_request.set_product_name("tomato")
+        tomato_request.set_quantity(3)
+
+        request = SellItemsRequest()
+        request.set_requests([salad_request, tomato_request])
+
+        self.use_case.run(request)
+
+        inserted_order = self.order_repository.get_saved_order()
+
+        self.assertEqual(OrderStatus.CREATED, inserted_order.get_status())
+        self.assertEqual(decimal.Decimal("23.20"), inserted_order.get_total())
+        self.assertEqual(decimal.Decimal("2.13"), inserted_order.get_tax())
+        self.assertEqual(decimal.Decimal("2.13"), inserted_order.get_tax())
+        self.assertEqual("EUR", inserted_order.get_currency())
+        self.assertEqual(2, len(inserted_order.get_items()))
+
+        first_item = inserted_order.get_items()[0]
+        self.assertEqual("salad", first_item.get_product().get_name())
+        self.assertEqual(decimal.Decimal("3.56"), first_item.get_product().get_price())
+        self.assertEqual(2, first_item.get_quantity())
+        self.assertEqual(decimal.Decimal("7.84"), first_item.get_taxed_amount())
+        self.assertEqual(decimal.Decimal("0.72"), first_item.get_tax())
+
+        second_item = inserted_order.get_items()[1]
+        self.assertEqual("tomato", second_item.get_product().get_name())
+        self.assertEqual(decimal.Decimal("4.65"), second_item.get_product().get_price())
+        self.assertEqual(3, second_item.get_quantity())
+        self.assertEqual(decimal.Decimal("15.36"), second_item.get_taxed_amount())
+        self.assertEqual(decimal.Decimal("1.41"), second_item.get_tax())
+
+    def test_unknown_product(self):
+        request = SellItemsRequest()
+        request.set_requests([])
+        unknown_product_request = SellItemRequest()
+        unknown_product_request.set_product_name("unknown product")
+        request.get_requests().append(unknown_product_request)
+
+        with self.assertRaises(UnknownProductError):
+            self.use_case.run(request)
